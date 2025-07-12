73677c4b371e546e7526f6ba90ecfe75e2a94097
Iago-Pereira <iagopereira@outlook.com>
2025-07-11 23:18:33 -0300

    feat: model pipeline

--- churn_code/train.py
+++ churn_code/train.py
@@ -2,7 +2,8 @@
 
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn import model_selection, tree
+from sklearn import model_selection, tree, linear_model, metrics, pipeline
+from feature_engine import discretisation, encoding
 # %%
 
 df = pd.read_csv("../data/abt_churn.csv")
@@ -38,6 +39,7 @@ print(f"Taxa variável resposta Teste: {y_test.mean()}")
 
 X_train.isna().sum().sort_values(ascending=False)
 # %%
+# EXPLORE
 
 df_analise = X_train.copy()
 df_analise[target] = y_train
@@ -66,4 +68,56 @@ feature_importances = (pd.Series(arvore.feature_importances_, index=X_train.colu
 feature_importances['acum']=feature_importances[0].cumsum()
 feature_importances[feature_importances[0] > 0.01] # Selecionar pelo atributo com importancia maior do que 1%
 #feature_importances[feature_importances['acum'] < 0.96] # Selecionar pelo atributo com importancia acumulada até 95%
+# %%
+
+best_features = (feature_importances[feature_importances['acum'] < 0.96]['index']
+                 .tolist())
+
+best_features
+# %%
+# MODIFY
+
+# Discretizar
+tree_discretization = discretisation.DecisionTreeDiscretiser(variables=best_features,
+                                                             regression=False,
+                                                             bin_output='bin_number',
+                                                             cv=3)
+
+# OneHotEncoding
+onehot = encoding.OneHotEncoder(variables=best_features, ignore_format=True)
+
+# %%
+# MODEL
+
+reg = linear_model.LogisticRegression(penalty=None, random_state=42, max_iter=100000)
+
+model_pipeline = pipeline.Pipeline(
+    steps=[
+        ('Discretizar', tree_discretization),
+        ('Onehot', onehot),
+        ('Model', reg),
+    ]
+)
+
+model_pipeline.fit(X_train, y_train)
+# %%
+
+y_train_predict = model_pipeline.predict(X_train)
+y_train_prob = model_pipeline.predict_proba(X_train)[:,1]
+
+acc_train = metrics.accuracy_score(y_train, y_train_predict)
+auc_train = metrics.roc_auc_score(y_train, y_train_prob)
+
+print("Acurárcia Treino:", acc_train)
+print("AUC Treino:", auc_train)
+# %%
+
+y_test_predict = model_pipeline.predict(X_test)
+y_test_prob = model_pipeline.predict_proba(X_test)[:,1]
+
+acc_test = metrics.accuracy_score(y_test, y_test_predict)
+auc_test = metrics.roc_auc_score(y_test, y_test_prob)
+
+print("Acurárcia Teste:", acc_test)
+print("AUC Teste:", auc_test)
 # %%
